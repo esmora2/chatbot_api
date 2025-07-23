cbe8faca17081e1bc93f925ffaa7ed5ccf36dad5
esmora2 <esmora2@espe.edu.ec>
2025-07-23 11:07:02 -0500

    Update settings.py

--- chatbot_api/settings.py
+++ chatbot_api/settings.py
@@ -25,8 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3gca(w%0d+(+(1&=byvba)mpvabl(fijhvj+8#%fkxqc8=22+o'
 
+SECRET_KEY = os.getenv('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
